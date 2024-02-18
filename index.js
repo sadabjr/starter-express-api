6db33daa290096f79035e86fb357bd57cce24f0f
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2024-02-18 18:39:18 +0530

    new code

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+require("dotenv").config()
+
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about page");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`example app listening on port ${port}`);
+});
